3a0e8753f54ceda0b00ba87b593a18b8421b35aa
Ernest Jones <ernest.jones30@yahoo.com>
2024-10-12 14:43:10 -0400

    Project setup

---
+++ motive/src/App.tsx
@@ -0,0 +1,19 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import { Routes, Route, Link } from "react-router-dom";
+
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <Routes>
+        {/* <Route path="/" element={<Home />} />
+    <Route path="/login" element={<Login />} /> */}
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
