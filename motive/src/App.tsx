2dbb796f471677a0f76b81390df8fcaf9835efe0
Camden Wierengo <CamdenPlaedu@gmail.com>
2024-10-12 19:19:18 -0400

    maps will not work without the api key

--- motive/src/App.tsx
+++ motive/src/App.tsx
@@ -2,15 +2,19 @@ import { useState } from "react";
 import { Routes, Route, Link } from "react-router-dom";
 import HomePage from "./pages/home";
 import MotivePricingPage from "./pages/pricing";
-import RouteGuard from "./libs/routeGuard";
 import "./App.css";
-
+import ChatbotSearch from "./pages/maps";
+import ContactPage from "./pages/contact";
+import FeaturesPage from "./pages/features";
 function App() {
   return (
     <div>
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/pricing" element={<MotivePricingPage />} />
+        <Route path="/maps" element={<ChatbotSearch />} />
+        <Route path="/contact" element={<ContactPage />} />
+        <Route path="/features" element={<FeaturesPage />} />
       </Routes>
     </div>
   );
--- motive/src/pages/pricing.tsx
+++ motive/src/pages/pricing.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { Check, X, Menu, DollarSign, Zap, Rocket } from "lucide-react";
 import { Link } from "react-router-dom";
+import Navbar from "../components/navbar";
 
 export default function MotivePricingPage() {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
@@ -59,40 +60,8 @@ export default function MotivePricingPage() {
 
   return (
     <div className="min-h-screen bg-base-100">
-      {/* Navigation */}
-      <nav className="navbar bg-base-100 shadow-lg">
-        <div className="container mx-auto">
-          <div className="flex-1">
-            <Link
-              to="/home"
-              className="btn btn-ghost normal-case text-xl text-white"
-            >
-              Motive
-            </Link>
-          </div>
-          <div className="flex-none hidden md:block">
-            <ul className="menu menu-horizontal px-1">
-              <li>
-                <Link to="/">Home</Link>
-              </li>
-              <li>
-                <Link to="/features">Features</Link>
-              </li>
-              <li>
-                <Link to="/pricing">Pricing</Link>
-              </li>
-              <li>
-                <Link to="/contact">Contact</Link>
-              </li>
-            </ul>
-          </div>
-          <div className="flex-none md:hidden">
-            <button className="btn btn-square btn-ghost" onClick={toggleMenu}>
-              {isMenuOpen ? <X /> : <Menu />}
-            </button>
-          </div>
-        </div>
-      </nav>
+   
+   < Navbar />
 
       {/* Mobile menu */}
       {isMenuOpen && (
